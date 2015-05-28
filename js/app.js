8bd127f086e8b8007bb9ff3f0e75ee4ddfe3541e
Mahendhar <mahendarreddy@live.com>
2015-05-28 11:47:06 +0530

    Handled key presses

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-app.js
+function keyPress(key) {
+	if (["AC", "DEL", "="].indexOf($(key).text()) == -1) {
+		$('#preview').html($('#preview').html() + $(key).text());
+	}
+
+	else {
+		console.log($(key).text());
+	}
+}
+
+
+
+$(document).ready(function() {
+	$('.key').click(function(event){
+		keyPress(this);
+	});
+});
